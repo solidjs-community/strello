e8f3d1c960ab31050b439be4e37d52edeb11b9ac
Brenley Dueck <brenleydueck@gmail.com>
2024-03-09 19:39:25 -0600

    feat: new board screen

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 import { action, cache, redirect } from "@solidjs/router";
 import { db } from "./db";
-import { login, register, validateEmail, validatePassword } from "./server";
+import { createBoard, login, register, validateEmail, validatePassword } from "./server";
 import { getSession, logoutSession, setAuthOnResponse } from "./auth";
 
 export const getUser = cache(async () => {
@@ -42,3 +42,48 @@ export const logout = action(async () => {
   await logoutSession();
   return redirect("/login");
 });
+
+export const getBoards = cache(async () => {
+  "use server";
+  const user = await getUser();
+
+  return db.board.findMany({
+    where: {
+      accountId: user.id,
+    },
+  });
+
+}, 'get-boards');
+
+export const addBoard = action(async (formData: FormData) => {
+  "use server";
+
+  const user = await getUser();
+  const name = String(formData.get('name'));
+  const color = String(formData.get('color'));
+
+  const board = await db.board.create({
+    data: {
+      accountId: user.id,
+      name,
+      color
+    },
+  });
+
+  return redirect(`/board/${board.id}`);
+}, 'add-board');
+
+export const deleteBoard = action(async (boardId: number) => {
+  "use server";
+
+  const user = await getUser();
+
+  console.log('test');
+
+  await db.board.delete({
+    where: { id: boardId, accountId: user.id },
+  });
+
+  return redirect('/');
+}, 'delete-board');
+
