941f3f565a45a87432764b49a825de3a2d5416b0
Daniel Afonso <danieljcafonso@gmail.com>
2024-04-30 17:52:12 +0100

    feat: update server preset to netlify deploy

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "@solidjs/start/config";
 import unocss from "unocss/vite";
 
 export default defineConfig({
+  server: {
+    preset: "netlify",
+  },
   vite: {
     plugins: [unocss()],
     ssr: { external: ["@prisma/client"] },
