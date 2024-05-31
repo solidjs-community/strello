270d79df656ee823f1610fa0d3f8cff2a40e65e9
Brenley Dueck <brenleydueck@gmail.com>
2024-05-31 18:21:33 -0500

    feat: support turso db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-const db = new PrismaClient();
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
+
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+const db = new PrismaClient({ adapter })
 
 process.on("beforeExit", () => {
   db.$disconnect();
